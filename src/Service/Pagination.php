56f64721f23dbb28535cd5c38191e3b22734505f
Thomas <tg.59290@gmail.com>
2019-02-11 19:59:26 +0100

    add pagination service

---
+++ src/Service/Pagination.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\BookRepository;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+
+class Pagination
+{
+    private $repository;
+    private $urlGenerator;
+
+    private $nextUrl;
+    private $previousUrl;
+    private $min;
+    private $max;
+    private $pages;
+    private $result;
+
+    const NUMBER_RESULTS = 10;
+
+    public function setPages($category = null) {
+      $numberEntities = $this->repository->createQueryBuilder('b');
+      if($category) {
+        $numberEntities = $numberEntities->addSelect("c")
+        ->leftJoin("b.category", "c")
+        ->andWhere('c.id = :id')
+        ->setParameter('id', $category->getId());
+      }
+            $numberEntities = $numberEntities->select('count(b.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+      $this->pages = ceil($numberEntities/self::NUMBER_RESULTS);
+    }
+
+    public function makeUrls($route, $page) {
+      if($page + 1 > $this->pages) {
+        $this->nextUrl = $this->urlGenerator->generate($route, ['page' => $page]);
+      }
+      else {
+        $this->nextUrl = $this->urlGenerator->generate($route, ['page' => $page + 1]);
+      }
+      if(($page - 1) > 0) {
+        $this->previousUrl = $this->urlGenerator->generate($route, ['page' => $page - 1]);
+      }
+      else {
+        $this->previousUrl = $this->urlGenerator->generate($route, ['page' => $page]);
+      }
+    }
+
+    public function makeRange($page) {
+      $this->max = $page * self::NUMBER_RESULTS ;
+      $this->min = $this->max - self::NUMBER_RESULTS;
+    }
+
+    public function makePage($route, $page, $category = null) {
+      $this->setPages($category);
+      $this->makeUrls($route, $page);
+      $this->makeRange($page);
+      $this->result = $this->repository->findBooksAndCategory($this->min, $this->max, $category);
+    }
+
+    public function getResult() {
+      return $this->result;
+    }
+
+    public function getNextUrl() {
+      return $this->nextUrl;
+    }
+
+    public function getPreviousUrl() {
+      return $this->previousUrl;
+    }
+
+    public function __construct(BookRepository $repository, UrlGeneratorInterface $urlGenerator) {
+      $this->repository = $repository;
+      $this->urlGenerator = $urlGenerator;
+    }
+}
